80ee8803c6afdea92fb75d3d723aa663d6ad6a80
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2023-10-02 16:28:45 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        // CHANGE THIS TO YOUR TENANT ID
+        private const string Tenant = "4523da32-a387-41b8-a85e-d6a051147706";
+        const string scope = "https://vault.azure.net/.default";
+        static async Task Main(string[] args)
+        {
+            var cancellationTokenSource = new CancellationTokenSource(10000); // 10 seconds timeout
+            var cred = new ChainedTokenCredential(
+                new AzureCliCredential(new AzureCliCredentialOptions
+                {
+                    TenantId = Tenant,
+
+                }),
+                new VisualStudioCredential(new VisualStudioCredentialOptions
+                {
+                    TenantId = Tenant
+                })
+            );
+            var token = await cred.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { scope }), cancellationTokenSource.Token);
+            Console.WriteLine(token.Token);
+        }
+    }
+}
